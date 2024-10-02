2809c5d39ac09165f157e1e206a2cfa76be17340
Anant Verma <E088088@mcgladrey.rsm.net>
2024-10-02 19:20:22 +0530

    feed

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js"
+import feedReducer from "./feedSlice";
+
 const appStore = configureStore({
 reducer:{
     user: userReducer,
+    feed: feedReducer,
+
 }
 })
 
