43aa80c41539e683a60838534b4f1a2adb54d2ec
Anant Verma <E088088@mcgladrey.rsm.net>
2024-10-02 23:10:23 +0530

    All done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,20 @@
-/* eslint-disable no-unused-vars */
-import axios from "axios";
 import { useState } from "react";
+import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("anant@gmail.com");
-  const [password, setPassword] = useState("Anant@123");
-  const [error, setRrror] = useState("")
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleLogin = async () => {
     try {
       const res = await axios.post(
@@ -23,31 +26,69 @@ const Login = () => {
         { withCredentials: true }
       );
       dispatch(addUser(res.data));
-      navigate("/feed");
+      return navigate("/feed");
     } catch (err) {
-      console.log(err);
-      setRrror(err?.response?.data || "Something went wrong!!!");
-      console.error(err);
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
     }
   };
 
   return (
     <div className="flex justify-center my-10">
-      <div className="card bg-base-200 w-96 shadow-xl">
+      <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
-          <div className="m">
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
+          <div>
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">First Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+              </>
+            )}
             <label className="form-control w-full max-w-xs my-2">
               <div className="label">
-                <span className="label-text">Email Id</span>
+                <span className="label-text">Email ID:</span>
               </div>
               <input
-                onChange={(e) => {
-                  setEmailId(e.target.value);
-                }}
-                value={emailId}
                 type="text"
+                value={emailId}
                 className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
               />
             </label>
             <label className="form-control w-full max-w-xs my-2">
@@ -55,27 +96,34 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </div>
               <input
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
                 type="password"
+                value={password}
                 className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </label>
           </div>
-          <p className="text-red-500">
-            {error}
-          </p>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
-            <button onClick={handleLogin} className="btn btn-primary">
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
   );
 };
-
 export default Login;
